ee71534f4a19f9c5f17623109e51f55ef957d035
kobohiram <komaki@hiramu.net>
2023-07-16 23:00:15 +0900

    Initial commit

---
+++ app/src/main/java/jp/teckacademy/yuka/komaki/taskapp2/MainActivity.kt
@@ -0,0 +1,21 @@
+package jp.teckacademy.yuka.komaki.taskapp2
+
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity
+import jp.teckacademy.yuka.komaki.taskapp2.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view=binding.root
+        setContentView(view)
+
+        binding.fab.setOnClickListener { v ->
+            Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
+                .setAction("Action", null).show()
+        }
+    }
+}
