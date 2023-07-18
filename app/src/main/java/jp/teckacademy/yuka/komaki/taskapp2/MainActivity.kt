a05856de5b363a7cd8731e029ab1f1e1683ce35b
kobohiram <komaki@hiramu.net>
2023-07-18 17:46:11 +0900

    InputActivity.ktの ３１行目、３７行目などの toolbar   content などが 解決できません。

--- app/src/main/java/jp/teckacademy/yuka/komaki/taskapp2/MainActivity.kt
+++ app/src/main/java/jp/teckacademy/yuka/komaki/taskapp2/MainActivity.kt
@@ -1,21 +1,111 @@
 package jp.teckacademy.yuka.komaki.taskapp2
 
+import android.content.Intent
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.snackbar.Snackbar
+import io.realm.kotlin.Realm
+import io.realm.kotlin.RealmConfiguration
+import io.realm.kotlin.delete
+import io.realm.kotlin.ext.query
+import io.realm.kotlin.notifications.InitialResults
+import io.realm.kotlin.notifications.UpdatedResults
+import io.realm.kotlin.query.Sort
 import jp.teckacademy.yuka.komaki.taskapp2.databinding.ActivityMainBinding
+import kotlinx.coroutines.*
+import java.text.SimpleDateFormat
+import java.util.*
+
+
+const val EXTRA_TASK ="jp.teckacademy.yuka.komaki.taskapp2.TASK"
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+
+    private lateinit var taskAdapter: TaskAdapter
+    private lateinit var realm: Realm
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        val view=binding.root
-        setContentView(view)
+        setContentView(binding.root)
 
-        binding.fab.setOnClickListener { v ->
-            Snackbar.make(v, "Replace with your own action", Snackbar.LENGTH_LONG)
-                .setAction("Action", null).show()
+        binding.fab.setOnClickListener {
+            val intent = Intent(this,InputActivity::class.java)
+            startActivity(intent)
+        }
+
+        // TaskAdapterを生成し、ListViewに設定する
+        taskAdapter = TaskAdapter(this)
+        binding.listView.adapter = taskAdapter
+
+        // ListViewをタップしたときの処理
+        binding.listView.setOnItemClickListener { parent, _, position, _ ->
+            val task=parent.adapter.getItem(position)as Task
+            val intent=Intent(this,InputActivity::class.java)
+            intent.putExtra(EXTRA_TASK,task.id)
+            startActivity(intent)
+        }
+
+        // ListViewを長押ししたときの処理
+        binding.listView.setOnItemLongClickListener { parent, _, position, _ ->
+            val task=parent.adapter.getItem(position)as Task
+            val builder = AlertDialog.Builder(this)
+
+            builder.setTitle("削除")
+            builder.setMessage(task.title+"を削除しますか")
+            builder.setPositiveButton("OK"){_, _ ->
+                realm.writeBlocking {
+                    val tasks=query<Task>("id==${task.id}").find()
+                    tasks.forEach{
+                        delete(it)
+                    }
+                }
+            }
+            builder.setNegativeButton("CANCEL",null)
+            val dialog=builder.create()
+            dialog.show()
+
+            true
+        }
+
+
+
+        // Realmデータベースとの接続を開く
+        val config = RealmConfiguration.create(schema = setOf(Task::class))
+        realm = Realm.open(config)
+
+        // Realmからタスクの一覧を取得
+        val tasks = realm.query<Task>().sort("date", Sort.DESCENDING).find()
+
+        // Realmが起動、または更新（追加、変更、削除）時にreloadListViewを実行する
+        CoroutineScope(Dispatchers.Default).launch {
+            tasks.asFlow().collect {
+                when (it) {
+                    // 更新時
+                    is UpdatedResults -> reloadListView(it.list)
+                    // 起動時
+                    is InitialResults -> reloadListView(it.list)
+                    else -> {}
+                }
+            }
+        }
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+
+        // Realmデータベースとの接続を閉じる
+        realm.close()
+    }
+
+    /**
+     * リストの一覧を更新する
+     */
+    private suspend fun reloadListView(tasks:List<Task>){
+        withContext(Dispatchers.Main){
+            taskAdapter.updateTaskList(tasks)
         }
     }
 }
